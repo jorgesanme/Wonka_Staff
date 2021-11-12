c0428906480f64838bb4a00254470a3c36d75b7b
jorgesname <jorgesanme@gmail.com>
2021-11-12 13:36:32 +0000

    Iniciado el ViewModel con los dos metodos de bajada de datos

--- app/src/main/java/com/example/wonka_staff/MainActivity.kt
+++ app/src/main/java/com/example/wonka_staff/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.wonka_staff
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.wonka_staff.databinding.ActivityMainBinding
 import com.example.wonka_staff.models.Result
 import com.example.wonka_staff.repository.WillyWonkaAPI
+import com.example.wonka_staff.viewModel.StaffViewModel
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.CoroutineScope
@@ -12,14 +15,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
+import okhttp3.internal.notifyAll
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
     var page: Int = 1
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater).also { binding ->
+        binding = ActivityMainBinding.inflate(layoutInflater).also { binding ->
             setContentView(binding.root)
         }
 
@@ -27,48 +34,32 @@ class MainActivity : AppCompatActivity() {
             if (page < 20) {
                 page++
                 binding.page.text = page.toString()
+            }else{
+                Toast.makeText(binding.root.context,"Last page: ${page.toString()}", Toast.LENGTH_LONG).show()
+
             }
         }
-        binding.btPreviusPage.setOnClickListener{
-            if (page > 1){
+        binding.btPreviusPage.setOnClickListener {
+            if (page > 1) {
                 page--
                 binding.page.text = page.toString()
-
+            }else{
+                Toast.makeText(binding.root.context,"First page: ${page.toString()}", Toast.LENGTH_LONG).show()
             }
         }
 
-            CoroutineScope(Dispatchers.IO).launch() {
-                val staffList: MutableList<Result> = downloadStaffList(page) as MutableList<Result>
-                var filterList: MutableList<Result> = mutableListOf()
-                withContext(Dispatchers.Main) {
-                    val adapter = StaffRecycleAdapter()
-                    binding.staffRV.adapter = adapter
-                    adapter.staffList = staffList
-                    adapter.notifyDataSetChanged()
-                    filterList = staffList.filter { it.gender.equals('F') } as MutableList<Result>
-                    print(filterList)
-                }
-            }
+        val viewModel = ViewModelProvider(this).get(StaffViewModel::class.java)
+        val adapter = StaffRecycleAdapter()
+        viewModel.getStaffList(page)
+        viewModel.state.observe(this){ state ->
+            adapter.staffList = state.staffList as MutableList<Result>
         }
 
+        binding.staffRV.adapter = adapter
 
     }
 
+}
 
-    private suspend fun downloadStaffList(page: Int): List<Result> {
-        return withContext(Dispatchers.IO) {
-            val client = OkHttpClient().newBuilder().build()
-            val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas/")
-                .client(client)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build()
 
-            val api: WillyWonkaAPI = retrofit.create(WillyWonkaAPI::class.java)
-            val response = api.getStaffList("?page=$page")
-//            response!!.results.filter { it.gender.equals("M") }
-            response!!.results
-        }
-    }
 
